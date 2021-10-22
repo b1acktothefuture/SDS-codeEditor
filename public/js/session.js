ae4aa4c7a70c64b4a40143e3416433d487e8d54c
b1acktothefuture <mutyalwarsm19.extc@coep.ac.in>
2021-10-22 23:00:12 +0530

    added chat feature

--- public/js/session.js
+++ public/js/session.js
@@ -2,29 +2,55 @@ const { id, name } = Qs.parse(location.search, {
     ignoreQueryPrefix: true
 })
 
+document.getElementById("session").innerText = window.location.host + "/sessions?id=" + id
+document.getElementById("name").innerText = name
 
+var EditorClient = ot.EditorClient;
+var SocketIOAdapter = ot.SocketIOAdapter;
+var CodeMirrorAdapter = ot.CodeMirrorAdapter;
 
-var editor = ace.edit("editor");
-ace.config.set('basePath', 'https://pagecdn.io/lib/ace/1.4.12/ace.js')
-editor.setTheme("ace/theme/monokai");
-editor.getSession().setMode("ace/mode/javascript");
-
-const socket = io()
-socket.emit('joinSession', { name, id })
-
-socket.on("changedcode", newCode => {
-    before = editor.selection.toJSON();
-    editor.setValue(newCode)
-    editor.selection.fromJSON(before)
-})
-
-socket.on('sessionusers', ({ users }) => {
-    // outputUsers(users)
-})
-
-editor.on('change', function () {
-    if (editor.curOp && editor.curOp.command.name) {
-        var val = editor.getSession().getValue();
-        socket.emit('codeChanged', val);
-    }
+var socket = io.connect('http://localhost:3000');
+var codeEditor = CodeMirror.fromTextArea(document.getElementById('codeeditor'), {
+    lineNumbers: true,
+    matchBrackets: true,
+    mode: "python"
 });
+
+
+var sourceCode = document.getElementById("codeeditor").innerText;
+var cmClient;
+
+
+function init(str, revision, clients, serverAdapter) {
+    if (!sourceCode) {
+        codeEditor.setValue(str);
+    }
+    cmClient = window.cmClient = new EditorClient(
+        revision, clients, serverAdapter, new CodeMirrorAdapter(codeEditor)
+    );
+};
+
+var userMessage = function (name, text) {
+    return ('<li class="media"><div class="media-body"><div class="media">' +
+        '<div class="media-body">' +
+        '<b>' + name + '</b> : ' + text +
+        '<hr/> </div></div></div></li>'
+    );
+};
+
+var sendMessage = function () {
+    var userMessage = document.getElementById("user-send").value;
+    socket.emit('chatMessage', { message: userMessage, username: name });
+    document.getElementById("user-send").value = "";
+};
+
+socket.on('doc', function (obj) {
+    init(obj.str, obj.revision, obj.clients, new SocketIOAdapter(socket));
+});
+
+socket.on('chatMessage', function (obj) {
+    console.log(obj)
+    document.getElementById("user-messages").innerHTML += userMessage(obj.username, obj.message)
+})
+
+socket.emit('joinSession', { session: id, username: name });
